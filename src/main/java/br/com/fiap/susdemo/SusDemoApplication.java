1be0edd2a988d83fe893f75f219ff69e45db96b3
Tiago Ferrer <ferrertiago@gmail.com>
2025-07-04 19:04:02 -0300

    feat: add springboot init app

---
+++ src/main/java/br/com/fiap/susdemo/SusDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.susdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SusDemoApplication.class, args);
+    }
+
+}
